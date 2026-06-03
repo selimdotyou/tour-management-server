f08ff62cc4db8795fd8d36451c63b83df8f2d35f
selimdotme <selim2305101565@diu.edu.bd>
2026-06-03 13:46:26 -0600

    tour management server setup now

---
+++ eslint.config.mjs
@@ -0,0 +1,14 @@
+// @ts-check
+
+import js from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import tseslint from 'typescript-eslint';
+
+export default defineConfig({
+  files: ['**/*.{js,ts}'],
+  extends: [js.configs.recommended, tseslint.configs.recommended],
+  rules: {
+    "no-console": "warn",
+    "no-unused-vars": "warn",
+  },
+});
